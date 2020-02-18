e26ac674a833e7438fac99ed77d3c6a2a68ed7d8
Diego <diegodltl@gmail.com>
2020-02-18 07:02:21 -0500

    Render markdown posts in blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,44 @@
 import React from "react"
+import { graphql, useStaticQuery } from 'gatsby';
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Test</h1>
-  </Layout>
-)
+const IndexPage = () => {
+
+  const data = useStaticQuery(graphql`
+    query{
+      allMarkdownRemark{
+        edges{
+          node{
+            frontmatter{
+              title
+              date
+              subtitle
+            }
+          }
+        }
+      }
+    }
+    `)
+
+  console.log(data);
+  return(
+    <Layout>
+      <SEO title="Home" />
+      {data.allMarkdownRemark.edges.map(edge=>{
+        return(
+          <article key={edge.node.id}>
+            <h2>{edge.node.frontmatter.title}</h2>
+            {edge.node.frontmatter.subtitle? <p>{edge.node.frontmatter.subtitle}</p>: null}
+            <time>{edge.node.frontmatter.date}</time>
+          </article>
+        )
+      })}
+    </Layout>
+  )
+}
 
 export default IndexPage
--- src/posts/blog-post.md
+++ src/posts/blog-post.md
@@ -1,9 +1,10 @@
 ---
 title: "This is a test"
+subtitle: "This will be the subtitle"
 date: "2020-04-04"
 ---
 
-Hello this is a test
+# Hello My name is Diego
 
 1. One
 2. Two
