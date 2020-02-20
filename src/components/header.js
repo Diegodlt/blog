bd55671fc083e018d518391080e22085ddda0038
Diego <diegodltl@gmail.com>
2020-02-20 05:26:13 -0500

    Conditionally return the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,56 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Location } from '@reach/router'
 
 import "../styles/layout.scss"
 
-const Header = ({ siteTitle }) => (
+const Header = (props) => {
+
+const mainHeader = `
   <div className="nav-wrapper">
-    <header>
-        <h1>
-          <Link
-            to="/"
-          >
-            {siteTitle}
-          </Link>
-        </h1>
-        <p>Full Stack Developer</p>
-    </header>
-    <nav>
-      <Link to="/">Blog</Link>
-      <Link to="/about">About</Link>
-    </nav>
-  </div>
+`
+return(
+  <Location>
+      {({location})=>{
+        if(location.pathname === '/'){
+          return(
+            <div className="nav-wrapper">
+              <header>
+                <h1>
+                  <Link
+                    to="/"
+                  >
+                    {props.siteTitle}
+                  </Link>
+                </h1>
+                <p>Full Stack Developer</p>
+              </header>
+              <nav>
+                <Link to="/">Blog</Link>
+                <Link to="/page-2">About</Link>
+              </nav>
+            </div>
+          )
+        }else{
+          return(
+            <div className="nav-warpper">
+              <header>
+                <h1>
+                  <Link
+                    to="/"
+                  >
+                    {props.siteTitle}
+                  </Link>
+                </h1>
+              </header>
+            </div>
+          )
+        }
+      }}   
+  </Location>
 )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
